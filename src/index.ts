d7a29530df511b29e04d3fbe2498945f61263c91
ecthon.almeida <ecthon.almeida@hitechnologies.com>
2023-11-09 13:31:19 -0400

    feature: router post

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,58 @@
-import express from 'express'
-// import { PrismaClient } from '@prisma/client';
+import express, { response } from 'express'
+import { PrismaClient } from '@prisma/client';
+import { request } from 'http';
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+
+// async function main() {
+//     const task =await prisma.task.create({
+//         data: {
+//             title: 'New task test',
+//             completed: false
+//         }
+//     })
+//     console.log(task)
+// }
+
+// main()
+//     .then(async() => {
+//         await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//         console.log(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+//     })
+
+app.get('/', async(request, response) => {
+    const tasks = await prisma.task.findMany()
+    return response.json(tasks);    
+})
+
+app.post('/newtask', async (request, response) => {
+    const { title } = request.body;
+
+    if (!title) {
+        return response.status(400).json({ error: 'O campo "title" é obrigatório.' });
+    }
+
+    try {
+        const task = await prisma.task.create({
+            data: {
+                title,
+                completed: false
+            }
+        });
+
+        response.json(task);
+    } catch (error) {
+        response.status(500).json({ error: 'Erro ao criar uma nova tarefa.' });
+    }
+});
+
 
 const server = app.listen( 3000, () => 
 console.log(`
