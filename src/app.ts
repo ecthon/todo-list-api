e6e5c13f3f3e4c7ba2dda142249a7654a1144d3a
ecthon <ecthon@gmail.com>
2023-11-23 12:56:08 -0400

    export const app in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import bodyParse from 'body-parser'
 import taskRoutes from './routes/taskRoutes'
 
 
-export const app = express();
-const port = 3001
+const app = express()
 
 app.use(bodyParse.json())
 app.use('/tasks', taskRoutes)
+
+export default app
